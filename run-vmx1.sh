ef9e2ab2d190835b72baf646c93c05b94738b33d
hmali <hiteshsmali@gmail.com>
2017-01-29 13:54:45 -0500

    Update run-vmx1.sh

--- run-vmx1.sh
+++ run-vmx1.sh
@@ -2,8 +2,7 @@
 docker run --name vmx1 --rm --privileged --net=host \
   -v $PWD:/u:ro \
   --env TAR="vmx-bundle-16.1R2.11.tgz" \
-  --env CFG="vmx1.cfg" \
-  --env DEV="br0 br0" \
+  --env DEV="br0 br1 br2 br3" \
   --env VCPMEM="1000" \
   --env MEM="4000" \
   --env VCPU="3" \
