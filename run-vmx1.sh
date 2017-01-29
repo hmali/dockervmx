ace16d4c7443ee42f9dead6e80a2d46b9b1be60d
hmali <hiteshsmali@gmail.com>
2017-01-29 13:51:22 -0500

    Add files via upload

---
+++ run-vmx1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker run --name vmx1 --rm --privileged --net=host \
+  -v $PWD:/u:ro \
+  --env TAR="vmx-bundle-16.1R2.11.tgz" \
+  --env CFG="vmx1.cfg" \
+  --env DEV="br0 br0" \
+  --env VCPMEM="1000" \
+  --env MEM="4000" \
+  --env VCPU="3" \
+  -i -t hmali/vmx:latest
